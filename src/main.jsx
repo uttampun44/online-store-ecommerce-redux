54ab3431c2c744fd715aff5977d59aee9c4ae443
uttam pun <uttampun62@gmail.com>
2023-02-11 18:34:54 +0000

    provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { store} from './store/Store';
+import  store from './store/Store';
 import { Provider } from 'react-redux';
 
 
