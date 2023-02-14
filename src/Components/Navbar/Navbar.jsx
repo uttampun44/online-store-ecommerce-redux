05a241a45d832ef65ed5a2e1bbcaf1be36645ebc
uttam pun <uttampun62@gmail.com>
2023-02-14 08:16:18 +0000

    Navbar design

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import NavbarDesign from './Navbar.module.css'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { useSelector } from 'react-redux';
+
 
 function Navbar() {
   const cartcolor = {
     color: '#fff'
   }
+  const data = useSelector((state) =>{
+    return state.cart
+   })
+
   return (
     <div className={NavbarDesign}>
          <header className={NavbarDesign.head}>
@@ -13,6 +19,7 @@ function Navbar() {
                 <div className={NavbarDesign.mainheader}>
                     <div className={NavbarDesign.title}><h1>Online Shopping Store</h1></div>
                     <div className={NavbarDesign.input}><input/></div>
+                    <div><p>{data.length}</p></div>
                     <div className={NavbarDesign}><ShoppingCartIcon style={cartcolor}/></div>
                 </div>
             </nav>
