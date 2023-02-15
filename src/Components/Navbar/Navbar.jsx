609faa0910254995e37a78ae53671383962b0186
uttam pun <uttampun62@gmail.com>
2023-02-15 20:41:15 +0000

    Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import NavbarDesign from './Navbar.module.css'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useSelector } from 'react-redux';
-
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   const cartcolor = {
@@ -19,8 +19,12 @@ function Navbar() {
                 <div className={NavbarDesign.mainheader}>
                     <div className={NavbarDesign.title}><h1>Online Shopping Store</h1></div>
                     <div className={NavbarDesign.input}><input/></div>
-                    <div><p>{data.length}</p></div>
-                    <div className={NavbarDesign}><ShoppingCartIcon style={cartcolor}/></div>
+                    <div className={NavbarDesign.cartandprice}>
+                         <div className={NavbarDesign}><ShoppingCartIcon style={cartcolor}/></div>
+                         <div className={NavbarDesign.linkcart}><Link to='/cart'>Cart</Link></div>
+                        <div className={NavbarDesign.cartitem}><p>{data.length}</p></div>
+                    </div>
+                  
                 </div>
             </nav>
          </header>
